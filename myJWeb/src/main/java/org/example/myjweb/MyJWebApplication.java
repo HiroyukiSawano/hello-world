8c537b4fd6e7968bfd45e885e3b07b3834027633
zzh <zhouzihang@inspur.com>
2025-12-18 18:17:13 +0800

    123

---
+++ myJWeb/src/main/java/org/example/myjweb/MyJWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.myjweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJWebApplication.class, args);
+    }
+
+}
---
+++ myJWeb/src/test/java/org/example/myjweb/MyJWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myjweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
